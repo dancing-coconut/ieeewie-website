b3c0681b3ff0cd6a603f6c56ceb25018f803e821
MitanshiKshatriya <mitanshikshatriya@gmail.com>
2020-12-15 10:45:54 +0530

    removed modal bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,47 @@
 (function($) {
   "use strict";
 
+    //modal
+
+
+    const body = document.querySelector("body");
+    const modal = document.querySelector(".modal");
+    const modalButton = document.querySelector(".modal-button");
+    const closeButton = document.querySelector(".close-button");
+    const scrollDown = document.querySelector(".scroll-down");
+    let isOpened = false;
+    
+    const openModal = () => {
+      modal.classList.add("is-open");
+      body.style.overflow = "hidden";
+    };
+    
+    const closeModal = () => {
+      modal.classList.remove("is-open");
+      body.style.overflow = "initial";
+    };
+    
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > window.innerHeight / 3 && !isOpened) {
+        isOpened = true;
+       // scrollDown.style.display = "none";
+       // openModal();
+      }
+    });
+    
+    
+    //modalButton.addEventListener("click", openModal);
+    closeButton.addEventListener("click", closeModal);
+    
+    
+    
+    document.onkeydown = evt => {
+      evt = evt || window.event;
+      evt.keyCode === 27 ? closeModal() : false;
+    };
+    
+      //modal
+
   // Preloader (if the #preloader div exists)
   $(window).on('load', function() {
     if ($('#preloader').length) {
@@ -148,6 +189,9 @@
     $('.venobox').venobox({
       'share': false
     });
+    /**modal */
+    openModal();
+    /**modal */
   });
 
   // Testimonials carousel (uses the Owl Carousel library)
